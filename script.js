ce64239da87fd713f382bce456efc010c267b78b
Bharat Kashyap <bharatkashyap@outlook.com>
2017-02-20 19:28:30 +0530

    Blurring on menu

--- script.js
+++ script.js
@@ -2,17 +2,20 @@ var hamburger = document.getElementById("hamburger");
 var plate = document.getElementById("ham-container");
 var list = document.getElementById("alt-nav");
 var text = document.getElementById("text");
+var image = document.getElementById("img");
 
 function bringGrid()
 {
   list.style.width = "35%";
   text.style.opacity = 0.4;
+  image.style.opacity = 0.4;
 }
 
 function hideGrid()
 {
   list.style.width = "0";
   text.style.opacity = 1;
+  image.style.opacity = 1;
 }
 
 function callMenu()
